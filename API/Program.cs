0a2b925a368c61aa325eed754aa39be66b0a7a40
robertgruevski <robertgruevski@outlook.com>
2025-10-22 15:34:50 -0400

    9. Creating the Entity Framework DbContext class

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
